f60d4b2d7380b18843ef1127ad13b1d16c1c4ba8
unknown <phuonghoa1821cbn@gmail.com>
2024-04-06 18:23:37 +0700

    update server

--- index.js
+++ index.js
@@ -7,21 +7,21 @@ const AccountModel=require('./connect/connectDB')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use('/public', express.static(path.join(__dirname,'/public')))
-app.get('/login', function(req, res) {
-    var duongDan=path.join(__dirname,'login.html')
+app.get('/', function(req, res) {
+    var duongDan=path.join(__dirname,'/views/login.html')
     res.sendFile(duongDan)
 })
-app.get('/index', function(req, res) {
-    var duongDan=path.join(__dirname,'index.html')
+app.get('/trangchu', function(req, res) {
+    var duongDan=path.join(__dirname,'/views/index.html')
     res.sendFile(duongDan)
 })
-app.get('/register', function(req, res) {
-    var duongDan=path.join(__dirname,'register.html')
+app.get('/dangky', function(req, res) {
+    var duongDan=path.join(__dirname,'/views/register.html')
     res.sendFile(duongDan)
 })
 
 
-app.post('/register',(req,res,next)=>{
+app.post('/dangky',(req,res,next)=>{
     var username=req.body.username
     var password=req.body.password
     var email=req.body.email
@@ -32,11 +32,11 @@ app.post('/register',(req,res,next)=>{
     })
     .then(data=>{
         if(data){
-            var duongDan=path.join(__dirname,'register')
+            var duongDan=path.join(__dirname,'/views/register.html')
             res.sendFile(duongDan)
         }
         else{
-            var duongDan=path.join(__dirname,'login.html')
+            var duongDan=path.join(__dirname,'/views/login.html')
             res.sendFile(duongDan)
             return AccountModel.create({
                 username:username,
@@ -53,7 +53,7 @@ app.post('/register',(req,res,next)=>{
     })
 })
 
-app.post('/login',(req,res,next)=>{
+app.post('/',(req,res,next)=>{
     var username=req.body.username
     var password=req.body.password
 
@@ -64,12 +64,14 @@ app.post('/login',(req,res,next)=>{
         
     .then(data=>{
         if(data){
-            console.log(username)
-            var duongDan=path.join(__dirname,'index.html')
+            console.log('2')
+            var duongDan=path.join(__dirname,'/views/index.html')
             res.sendFile(duongDan)
         }
         else{
-            console.log(password)
+            console.log('1')
+            var duongDan=path.join(__dirname,'/views/login.html')
+            res.sendFile(duongDan)
             res.status(300).json('Thông tin tài khoản hoặc mật khẩu không chính xac')
         }
     })
@@ -80,9 +82,6 @@ app.post('/login',(req,res,next)=>{
 })
 
 
-app.get('/',(req,res,next)=>{
-    res.json('HOME')
-})
 
 
 app.listen(3333,function(){

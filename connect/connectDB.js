0df8d3896368152dfca6751ae0d8546be7f0288e
unknown <phuonghoa1821cbn@gmail.com>
2024-04-06 17:21:19 +0700

    xu ly dang nhap

--- connect/connectDB.js
+++ connect/connectDB.js
@@ -7,13 +7,16 @@ const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/webproject',{
   useNewUrlParser:true,
   useUnifiedTopology:true
-});
+})
+.then(() => console.log('Kết nối thành công!'))
+.catch(err => console.error('Lỗi kết nối:', err));
 
 const Schema=mongoose.Schema;
 
 const AccountSchema= new Schema({
   username: String,
   password: String,
+  email:String
 },{
   collection:'phuong'
 });
--- index.js
+++ index.js
@@ -7,43 +7,82 @@ const AccountModel=require('./connect/connectDB')
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 app.use('/public', express.static(path.join(__dirname,'/public')))
-app.get('/', function(req, res) {
+app.get('/login', function(req, res) {
     var duongDan=path.join(__dirname,'login.html')
     res.sendFile(duongDan)
 })
-app.get('/trangchu', function(req, res) {
+app.get('/index', function(req, res) {
     var duongDan=path.join(__dirname,'index.html')
+    res.sendFile(duongDan)
+})
+app.get('/register', function(req, res) {
+    var duongDan=path.join(__dirname,'register.html')
     res.sendFile(duongDan)
 })
 
 
-app.post('/',(req,res,next)=>{
+app.post('/register',(req,res,next)=>{
+    var username=req.body.username
+    var password=req.body.password
+    var email=req.body.email
+    var rpassword=req.body.rpassword
+
+    AccountModel.findOne({
+        username:username
+    })
+    .then(data=>{
+        if(data){
+            var duongDan=path.join(__dirname,'register')
+            res.sendFile(duongDan)
+        }
+        else{
+            var duongDan=path.join(__dirname,'login.html')
+            res.sendFile(duongDan)
+            return AccountModel.create({
+                username:username,
+                password:password,
+                email:email    
+            })
+                
+            
+        }
+    })
+   
+    .catch(err=>{
+        res.status(500).json('Tạo tài khoản thất bại')
+    })
+})
+
+app.post('/login',(req,res,next)=>{
     var username=req.body.username
     var password=req.body.password
 
-    AccountModel.create({
+    AccountModel.findOne({
         username:username,
         password:password
     })
+        
     .then(data=>{
-        res.json('thanh cong')
+        if(data){
+            console.log(username)
+            var duongDan=path.join(__dirname,'index.html')
+            res.sendFile(duongDan)
+        }
+        else{
+            console.log(password)
+            res.status(300).json('Thông tin tài khoản hoặc mật khẩu không chính xac')
+        }
     })
+ 
     .catch(err=>{
-        res.status(500).json('that bai')
+        res.status(500).json('Server bị lỗi')
     })
 })
+
 
 app.get('/',(req,res,next)=>{
     res.json('HOME')
 })
-
-
-
-
-
-
-
-
 
 
 app.listen(3333,function(){
